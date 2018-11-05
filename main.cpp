8333376c9cfa6241827d2f0b1b5a6a67b01a2c1b
Midelcas <44088960+Midelcas@users.noreply.github.com>
2018-11-05 17:11:02 +0100

    Analog thread light sensor added

--- main.cpp
+++ main.cpp
@@ -4,6 +4,7 @@
 #include "TCS34725.h"
 #include "MMA8451Q.h"
 #include "Si7021.h"
+#include "HW5P-1.h"
 #define OFF 0
 #define ON 1
 #define TIMEOUT_TEST_MODE    2
@@ -18,12 +19,10 @@ InterruptIn sw1(PB_2);
 DigitalOut testMode(LED1);
 DigitalOut normalMode(LED2);
 
-//extern Thread threadANALOG;
-
-//extern void ANALOG_thread();
+extern Thread threadANALOG;
+extern void ANALOG_thread();
 
 extern Thread threadI2C;
-
 extern void I2C_thread();
 
 extern float valueSM;
@@ -31,6 +30,7 @@ extern float valueSM;
 extern ColorData colorData;
 extern AccelerometerData accData;
 extern AmbientData ambData;
+extern LightData lightData;
 Timeout to;
 bool writeTime;
 int count;
@@ -83,11 +83,13 @@ int main() {
 					pc.printf("\nClear (%d)Red (%d), Green (%d), Blue (%d) \n",colorData.clear_value, colorData.red_value, 
 								colorData.green_value, colorData.blue_value);
 					leds = colorData.dominant;
+					pc.printf("\nLight (%f)\n",lightData.light);
 				}else if(normalMode==ON){
 					pc.printf("\nTemp: (%.2fºC),Hum: (%.2f%%)\n",ambData.temperature, ambData.humidity);
 					pc.printf("\nX (%f),Y (%f),Z (%f)\n",accData.x, accData.y, accData.z);
 					pc.printf("\nClear (%d)Red (%d), Green (%d), Blue (%d) \n",colorData.clear_value, colorData.red_value, 
 								colorData.green_value, colorData.blue_value);
+					pc.printf("\nLight (%f)\n",lightData.light);
 					if(count==120){
 						pc.printf("\nMaxH: (%.2f%%),MinH: (%.2f%%)\n",ambData.maxHumidity, ambData.minHumidity);
 						pc.printf("\nMaxT: (%.2fºC),MinT: (%.2fºC)\n",ambData.maxTemperature, ambData.minTemperature);
@@ -96,6 +98,8 @@ int main() {
 						pc.printf("\nMaxX (%f),MaxY (%f),MaxZ (%f)\n",accData.x_Max, accData.y_Max, accData.z_Max);
 						pc.printf("\nMinX (%f),MinY (%f),MinZ (%f)\n",accData.x_Min, accData.y_Min, accData.z_Min);
 						pc.printf("\nDominant Color:%s\n",colorData.hourDominant.c_str());
+						
+						pc.printf("\nMax Light (%f), Min Light (%f), Mean Light (%f)\n",lightData.maxLight, lightData.minLight, lightData.meanLight);
 						//pc.printf("\nRedTimes (%d),GreenTimes (%d),BlueTimes (%d)\n",colorData.acc_red, colorData.acc_green, colorData.acc_blue);
 					}
 					
--- threadANALOG.cpp
+++ threadANALOG.cpp
@@ -1,20 +1,24 @@
 #include "mbed.h"
 #include "rtos.h"
+#include "HW5P-1.h"
 
 
-AnalogIn soilmois(PA_0); 
+//AnalogIn soilmois(PA_0); 
 
-float valueSM=0.0;
+//float valueSM=0.0;
 
+LightData lightData;
 Thread threadANALOG(osPriorityNormal, 512); // 1K stack size
 
 void ANALOG_thread(); 
+HW5P_1 *lightSensor=0;
 
 void ANALOG_thread() {
-		while (true) {
-
-				Thread::wait(200);
-				valueSM=soilmois*100;
+	lightSensor=new HW5P_1(PA_4);
+	while (true) {
+				lightData=lightSensor->measure();
+				wait(0.1);
+				//valueSM=soilmois*100;
 				  
     }
 }
